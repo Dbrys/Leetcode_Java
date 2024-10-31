ac06febc0feb1922d973fa9b02f89f424800972d
Dbrys <brysdillon@gmail.com>
2024-10-30 21:32:51 -0400

    clean up

---
+++ src/DynamicProgramming.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class DynamicProgramming {
+
+    public static void main(String[] args) {
+        //TESTS
+        List<String> wordDict = Arrays.asList("leet","code");
+        System.out.println(wordBreak("leetcode", wordDict));
+    }
+
+
+    //WORD_BREAK - leetcode 139:
+    public static boolean wordBreak(String s, List<String> wordDict){
+        Set<String> dictionary = new HashSet<>(wordDict);
+
+        boolean[] dp = new boolean[s.length() + 1];
+
+        // Empty string is always valid
+        dp[0] = true;
+
+        // For each position i in the string
+        for (int i = 1; i <= s.length(); i++) {
+            // Try all possible substrings ending at i
+            for (int j = 0; j < i; j++) {
+                // If we can segment string up to j AND substring from j to i is in dictionary
+                if (dp[j] && dictionary.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+
+}
