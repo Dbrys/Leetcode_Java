9a1bbc6d05792c6cb1c5ca7281a898a1bde5bee0
Dbrys <brysdillon@gmail.com>
2024-11-12 21:15:17 -0500

    unique char: problem 387

---
+++ src/Strings.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class Strings {
+
+    public static void main(String args[]){
+        //Test
+     System.out.println(uniqueCharacter("alias"));
+    }
+
+    private static int uniqueCharacter(String str) {
+        HashMap<Character,Integer> charMap = new HashMap<Character,Integer>();
+
+        for(char letter : str.toCharArray()){
+            charMap.put(letter, charMap.getOrDefault(letter,0) + 1);
+        }
+
+        for(int i = 0; i < str.length(); i++){
+            if(charMap.get(str.charAt(i)) == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
