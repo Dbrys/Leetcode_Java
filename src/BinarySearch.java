fcb973526e92dae6f89672faa4662e51be8a71c6
Dbrys <brysdillon@gmail.com>
2025-01-14 21:17:29 -0500

    bs find peaks

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -29,5 +29,22 @@ public class BinarySearch {
         }
         return successCountList;
     }
+
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int middle = (left + right) / 2;
+            System.out.println("value:" + middle);
+            if (nums[middle] > nums[middle + 1]) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+
+        }
+        return left;
+    }
 }
 
