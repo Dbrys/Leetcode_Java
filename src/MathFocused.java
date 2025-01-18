e6a779d2dd7cafd42e6f1f4a2dd0c620ec3b64cb
Dbrys <brysdillon@gmail.com>
2025-01-17 21:47:18 -0500

    pivot index

--- src/MathFocused.java
+++ src/MathFocused.java
@@ -35,4 +35,23 @@ public class MathFocused {
         }
         return Math.max(maxAltitude, 0);
     }
+
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            rightSum = totalSum - leftSum - nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
 }
