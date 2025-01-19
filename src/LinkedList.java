d163607ed88f026db11895fab63f9af5f3d04619
Dbrys <brysdillon@gmail.com>
2025-01-19 17:52:52 -0500

    odd even linked list

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -36,6 +36,25 @@ public class LinkedList {
         return prev;
     }
 
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+
     public static class ListNode {
         int val;
         ListNode next;
