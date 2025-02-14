25df33feb47559e9862cf87671e556c35d87bdeb
Dbrys <brysdillon@gmail.com>
2025-02-13 21:27:16 -0500

    valid binary search tree

--- src/DFS.java
+++ src/DFS.java
@@ -358,4 +358,21 @@ public class DFS {
 
         curPath.remove(curPath.size() - 1);
     }
+
+    //##############
+    public boolean isValidBST(TreeNode root) {
+        return bstIsValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean bstIsValid(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        return bstIsValid(node.left, min, node.val) && bstIsValid(node.right, node.val, max);
+    }
 }
